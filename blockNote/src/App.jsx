aff87899603a5fb48a5585beb9097e5c5de374d7
홍범선 <qjatjs123123@naver.com>
2024-10-25 16:52:54 +0900

    123

--- blockNote/src/App.jsx
+++ blockNote/src/App.jsx
@@ -9,7 +9,7 @@ import * as Y from "yjs";
 // Sets up Liveblocks client.
 const client = createClient({
   publicApiKey:
-    "pk_dev_lJAS4XHx3l1e0x_Gh9VMtrvo8PEB1vrNarC2YRtAOp4t6i9_QAcSX2U953GS6v7B",
+    "pk_prod_fAxHJojd9L1CSsl-bTxSuSkJ-REmKxV5DOe2KfgNCCHgTpQxBQZ82dLQzP5qdzOJ",
 });
 // Enters a multiplayer room.
 // Use a unique name as a "room" for your application.
