1a6742c82a80724089ea3ef13d0ed6f9642aa956
홍범선 <qjatjs123123@naver.com>
2024-10-25 16:44:02 +0900

    123

--- blockNote/src/App.jsx
+++ blockNote/src/App.jsx
@@ -1,13 +1,42 @@
-
-import './App.css'
+import "@blocknote/core/fonts/inter.css";
 import { useCreateBlockNote } from "@blocknote/react";
 import { BlockNoteView } from "@blocknote/mantine";
-import "@blocknote/core/fonts/inter.css";
 import "@blocknote/mantine/style.css";
-function App() {
-  const editor = useCreateBlockNote();
-
+import { createClient } from "@liveblocks/client";
+import {LiveblocksYjsProvider} from "@liveblocks/yjs";
+import * as Y from "yjs";
+ 
+// Sets up Liveblocks client.
+const client = createClient({
+  publicApiKey:
+    "pk_dev_lJAS4XHx3l1e0x_Gh9VMtrvo8PEB1vrNarC2YRtAOp4t6i9_QAcSX2U953GS6v7B",
+});
+// Enters a multiplayer room.
+// Use a unique name as a "room" for your application.
+const { room } = client.enterRoom("your-project-name", {
+  initialPresence: {},
+});
+ 
+// Sets up Yjs document and Liveblocks Yjs provider.
+const doc = new Y.Doc();
+const provider = new LiveblocksYjsProvider(room, doc);
+ 
+export default function App() {
+  const editor = useCreateBlockNote({
+    collaboration: {
+      // The Yjs Provider responsible for transporting updates:
+      provider,
+      // Where to store BlockNote data in the Y.Doc:
+      fragment: doc.getXmlFragment("document-store"),
+      // Information (name and color) for this user:
+      user: {
+        name: "My Username",
+        color: "#ff0000",
+      },
+    },
+  });
+ 
+  // Renders the editor instance.
   return <BlockNoteView editor={editor} />;
 }
-
-export default App
+ 
